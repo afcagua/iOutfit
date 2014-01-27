341732d0efe18d8ddd4238ac9324d5193b8f5717
Andres Cagua <caguasaki300@hotmail.com>
2014-01-26 23:36:10 -0500

    Activities y clases

--- iOutfit/gen/com/example/ioutfit/R.java
+++ iOutfit/gen/com/example/ioutfit/R.java
@@ -24,10 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int imageView1=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_capture_clothes=0x7f030000;
+        public static final int activity_consultent=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_profile=0x7f030003;
+        public static final int activity_settings=0x7f030004;
+        public static final int activity_wardrobe=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,6 +43,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_capture_clothes=0x7f050005;
+        public static final int title_activity_consultent=0x7f050004;
+        public static final int title_activity_profile=0x7f050006;
+        public static final int title_activity_settings=0x7f050007;
+        public static final int title_activity_wardrobe=0x7f050003;
     }
     public static final class style {
         /** 
